be32e977e972e7dd061378c48cee7dc44ad65309
Mayank Kumar Patel <72437985+night05fury@users.noreply.github.com>
2023-06-18 19:01:42 +0530

    Added ABout me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,25 +9,18 @@ const About = () => {
                  About</p>
             </div>
             <p className="text-xl mt-20">
-            Harsha is most concerned about that his being traced in the internet because he is 
-            getting lot of spam messages from the websites he is visiting regarding his location 
-            or regarding his device like lot of adware in the internet which some time be tempting 
-            to click and sending your data in the hands of malicious persons over internet, so help 
-            Harsha to hide himself from the internet so that no website can trace Harsha location 
-            stats and send spam messages
+                I'm Mayank Kumar Patel, a passionate web developer with a love for gaming and gardening. 
+                With a strong background in web development and a knack for creating user-friendly and visually appealing websites, 
+                I thrive in the ever-evolving digital landscape.
+                Let me take you through my skills as a web developer and about myself.
             </p>
 
             <br/>
             
             <p className="text-xl">
-            The surface web refers to the portion of the World Wide Web that is accessible to the general public 
-            through conventional search engines or browsers. It includes websites and web pages that are indexed 
-            and easily found through search engines like Google, Bing, or Yahoo, and can be accessed without any 
-            specialized software or authorization.
-            The surface web is the largest and most well-known part of the internet, and it includes a vast range 
-            of websites, such as news sites, social media platforms, online shopping sites, and information portals. 
-            It is estimated that the surface web represents only a small fraction of the entire internet, with the majority 
-            of the content remaining hidden in the deeper, non-indexed parts of the web known as the "deep web" or "dark web".
+                I am a graduate of the VIT Bhopal University with a Bachelor of Technology in Computer Science and Engineering with Specialisation in Gaming Technology.
+                As an avid gamer, I immerse myself in various genres and platforms, appreciating the artistry and storytelling within games. 
+                Gaming has not only sharpened my problem-solving and critical thinking skills but has also sparked my creativity in designing user experiences.
             </p>
         </div>
     </div>
